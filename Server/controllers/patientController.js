1baa610736b7f96bc372dd4d9cb3863bb36a7395
Gautam-2407 <gautambhatia2407@gmail.com>
2023-12-04 18:51:41 +0530

    Start

---
+++ Server/controllers/patientController.js
@@ -0,0 +1,58 @@
+const patient = require("../models/patientSchema");
+
+exports.patientregister = async (req, res) => {
+    const {name, phone, email, birth, gender, description, password}=req.body;
+
+    if (!name || !phone || !email|| !birth|| !gender || !description || !password ){
+      return  res.status(401).json({message:"Fill all fields"})
+    }
+
+    try{
+      const prepatient = await patient.findOne({email:email});
+
+      if (prepatient){
+        return   res.status(200).json("patient already exist")
+      }
+      else{
+        const newpatient = new patient({
+          name,
+          phone,
+          email,
+          birth,
+          gender,
+          description,
+          password
+        });
+        const storeData= await newpatient.save();
+        res.status(200).json(storeData);
+      }
+    } catch (error) {
+        res.status(400).json({ error: "Invalid Details", error });
+      } 
+  };
+  
+  
+  //CHECK PHONE INFORMATION IN DATABASE
+  exports.patientlogin = async (req, res) => {
+     const { email } = req.body;
+  
+     const prepatient = await patient.findOne({ email: email });
+     
+     try{
+
+       if (!prepatient) {
+         return res.send("email not found")
+        }
+        else {
+          res.status(201).json({ exists: true , prepatient});
+          console.log("email match");
+        }
+      }
+    
+    catch (error) {
+      console.error("Error while querying MongoDB:", error);
+      res.status(500).json({ error: "Unable to connect with DB" });
+    }
+    
+  };
+  
